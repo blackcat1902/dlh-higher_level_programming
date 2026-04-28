49a6e24952290b78e078cda7a95dd385a258c269
blackcat1902 <113116015+blackcat1902@users.noreply.github.com>
2026-04-28 22:39:07 +0200

    Update 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-# using str1 and str2
-print("Welcome to {} {}!".format(str1, str2))
+str = "Holberton School"
+print(str + str + str)
+print(str[:9])
