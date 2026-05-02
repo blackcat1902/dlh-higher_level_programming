cbf22586354ccd84d74708faf2d7ece0e57cb5e2
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-02 21:49:33 +0000

    mo

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    for i in range(x):
+        try:
+            value = my_list[i]
+        except IndexError:
+            break
+
+        try:
+            print("{:d}".format(value), end="")
+            count += 1
+        except (ValueError, TypeError):
+            pass
+
+    print()
+    return count
+    
