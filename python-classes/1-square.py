c42da8d98e66104d3d4332fe7cc893d69fe0a6c1
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-05 16:33:08 +0000

    mo

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module defines a Square class."""
+
+
+class  Square:
+    """This class defines a square."""
+    def __init__(self,size):
+    self.__size = size
+    my_square = Square (3)
+    print("my_square")
+    
