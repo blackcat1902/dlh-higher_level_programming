ee0480f8bacb5382661abfada86d2f657e5132bf
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-05 19:37:33 +0000

    mo

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,21 @@
+
 #!/usr/bin/python3
-""" This module defines a Square class."""
+"""
+This module defines a Square class.
+The Square class is defined with a private size attribute.
+"""
 
 
 class Square:
-    """This class defines a square."""
-    def __init__(self,size):
+    """
+    This class defines a square with a private size attribute.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with a given size.
+
+        Args:
+            size: The size of the square (no type or value validation).
+        """
         self.__size = size
-my_square = Square (3)
-print(my_square)
-    
