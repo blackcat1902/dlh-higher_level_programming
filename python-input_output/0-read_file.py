0f7276bbd1f8b30b17d26acf1c27b6b3968e9ab2
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-10 07:48:58 +0000

    mo

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Module for reading a UTF-8 text file."""
 
+
 def read_file(filename=""):
     """Reads a text file and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-  
+        
