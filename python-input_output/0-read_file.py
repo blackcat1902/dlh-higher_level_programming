3e0189fc599562434a6095b8b6baef41a04c06d0
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-10 07:36:38 +0000

    mo

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,5 @@
 """Reads a text file (UTF8) and prints it to stdout."""
 
 def read_file(filename=""):
-    with open (filename, encoding="utf-8") as f:
-        print(f.read(), end= "")
-   
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
