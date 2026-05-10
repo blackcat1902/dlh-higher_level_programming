25950993a561987515ee7d18c21cad38a5756170
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-10 10:28:13 +0000

    mo

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for appending a string at the end of a UTF-8 text file."""
+
+
+def append_write(filename="", text=""):
+    """A function that appends a string at the end of a text file (UTF8)."""
+    with open(filename, "a", encoding= "UFT8") as f:
+        return f.write(text)
