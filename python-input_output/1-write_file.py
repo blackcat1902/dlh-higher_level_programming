00e59b8528fae46dc089b7faf3423816d07cd25f
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-10 10:18:12 +0000

    mo

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for writing a string to a text file a UTF-8 text file."""
+
+
+def write_file(filename="", text=""):
+    """A function that writes a string to a text file"""
+    with open(filename, "w" encoding="utf-8") as f :
+        return f.write(text) 
+        
