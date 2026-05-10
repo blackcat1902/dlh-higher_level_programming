98a9efbbd60fe48d33915370dffb0ebdb31d80dd
blackcat1902 <fatmacankutuk@gmail.com>
2026-05-10 07:42:10 +0000

    mo

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,10 +2,7 @@
 """Reads a text file (UTF8) and prints it to stdout."""
 
 def read_file(filename=""):
-    ""Reads a text file and prints it to stdout."""
-    with 
-
-    
-        
-
-    
+    """Reads a text file and prints it to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
+  
