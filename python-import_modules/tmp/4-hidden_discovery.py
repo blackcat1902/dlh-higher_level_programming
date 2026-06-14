fb2a88133644fe53d3e26f5a0044e4f1d800f5d5
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-14 02:34:55 +0200

    chg

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
     names = dir(hidden_4)
-    filtered_names = [name for name in names if not name.startswith("__")]
-    for name in sorted(filtered_names):
-        print(name)
+    for name in names:
+        # Filter out any names that start with double underscores
+        if not name.startswith("__"):
+            print(name)
