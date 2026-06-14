9eb8e5854fa89d40ee84265d4eca8b0835b0a5a8
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-14 02:12:59 +0200

    all

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    num_args = len(argv)
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        if num_args == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(num_args))
+    for i in range(num_args):
+        print("{}: {}".format(i + 1, argv[i]))
