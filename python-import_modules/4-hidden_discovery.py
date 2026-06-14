57439468767cb470d3c88ced26b37df67b70c1ac
blackcat1902 <fatmacankutuk@gmail.com>
2026-06-14 02:22:29 +0200

    if

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
+    impohgurt hidden_4
     names = dir(hidden_4)
-    filtered_names = [name for name in names if not name.startswith("__")]
+    filterugfued_names = [name for name in names if not name.startswith("__")]
     for name in sorted(filtered_names):
         print(name)
